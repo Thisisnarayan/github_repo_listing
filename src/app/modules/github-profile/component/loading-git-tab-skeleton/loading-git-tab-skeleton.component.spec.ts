9c0f99600379c6c7dbc2eb68259ad1dc87063248
Narayan Sharma <narayansharma1806@gmail.com>
2023-03-23 10:50:42 +0530

    ng test base case

--- src/app/modules/github-profile/component/loading-git-tab-skeleton/loading-git-tab-skeleton.component.spec.ts
+++ src/app/modules/github-profile/component/loading-git-tab-skeleton/loading-git-tab-skeleton.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CardModule } from 'primeng/card';
+import { SkeletonModule } from 'primeng/skeleton';
 
 import { LoadingGitTabSkeletonComponent } from './loading-git-tab-skeleton.component';
 
@@ -8,7 +10,8 @@ describe('LoadingGitTabSkeletonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoadingGitTabSkeletonComponent ]
+      declarations: [ LoadingGitTabSkeletonComponent ],
+      imports : [CardModule , SkeletonModule]
     })
     .compileComponents();
 
--- src/app/modules/github-profile/component/loading-profile-skeleton/loading-profile-skeleton.component.spec.ts
+++ src/app/modules/github-profile/component/loading-profile-skeleton/loading-profile-skeleton.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CardModule } from 'primeng/card';
+import { SkeletonModule } from 'primeng/skeleton';
 
 import { LoadingProfileSkeletonComponent } from './loading-profile-skeleton.component';
 
@@ -8,7 +10,8 @@ describe('LoadingProfileSkeletonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoadingProfileSkeletonComponent ]
+      declarations: [ LoadingProfileSkeletonComponent ],
+      imports : [CardModule , SkeletonModule]
     })
     .compileComponents();
 
